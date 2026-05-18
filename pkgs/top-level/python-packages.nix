88f62d6d85c60aec864691bbed73b5707c07d292
Martin Weinelt <hexa@darmstadt.ccc.de>
2026-05-19 01:08:40 +0200

    python3Packages.distutils_80: init at 80.10.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4081,6 +4081,9 @@ self: super: with self; {
   distutils =
     if pythonOlder "3.12" then null else callPackage ../development/python-modules/distutils { };
 
+  distutils_80 =
+    if pythonOlder "3.12" then null else callPackage ../development/python-modules/distutils/80.nix { };
+
   distutils-extra = callPackage ../development/python-modules/distutils-extra { };
 
   dj-database-url = callPackage ../development/python-modules/dj-database-url { };
